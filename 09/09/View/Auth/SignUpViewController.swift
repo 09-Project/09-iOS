5e1d8985e09af87435cee5a05978510f99439df8
KimKiYoung <rlarldud6482@naver.com>
2021-10-02 22:09:08 +0900

    [Create] SignUpViewController

---
+++ 09/09/View/Auth/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignInViewController.swift
+//  09
+//
+//  Created by 김기영 on 2021/10/02.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
