f60019962dd15db27e5b808f396e9e23603d82a7
KimKiYoung <rlarldud6482@naver.com>
2021-10-05 19:09:20 +0900

    [Create] Header

--- 09/09/Networking/Header.swift
+++ 09/09/Networking/Header.swift
@@ -7,3 +7,10 @@
 
 import Foundation
 import Alamofire
+
+enum API {
+    case signUP(_ name: String, _ username: String, _ password: String)
+    case signIn(_ username: String, _ password: String)
+    case reissue(_ access_token: String, _ refresh_token: String)
+    case changepw(_ password: String, _ new_password: String)
+}
