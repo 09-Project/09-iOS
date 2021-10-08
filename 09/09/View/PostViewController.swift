08ec9fd362a04ca4037749aab2b2a12eee9f1039
KimKiYoung <rlarldud6482@naver.com>
2021-10-08 15:07:50 +0900

    [Create] PostViewController

---
+++ 09/09/View/PostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PostViewController.swift
+//  09
+//
+//  Created by 김기영 on 2021/10/08.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
