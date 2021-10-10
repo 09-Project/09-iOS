cbc092bbcfa1b8c535afff46ffaab5c67a03e68a
KimKiYoung <rlarldud6482@naver.com>
2021-10-10 22:08:45 +0900

    [Update] MyPageViewController

--- 09/09/View/MyPageViewController.swift
+++ 09/09/View/MyPageViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Tabman
 
 class MyPageViewController: UIViewController {
     
@@ -25,7 +26,7 @@ class MyPageViewController: UIViewController {
     }
     
     private lazy var gearBtn = UIButton().then {
-        $0.setImage(.init(systemName: "gear"), for: .normal)
+        $0.setImage(.init(named: "톱니바퀴.PNG.png"), for: .normal)
         $0.tintColor = .init(named: "mainColor")
     }
     
@@ -110,23 +111,6 @@ class MyPageViewController: UIViewController {
         $0.textColor = .init(named: "mainColor")
     }
     
-    private lazy var btn1 = UIButton().then {
-        $0.backgroundColor = .white
-        $0.titleLabel!.font = .init(name: fontMedium, size: 15)
-        $0.setTitle("상품", for: .normal)
-    }
-    
-    private lazy var btn2 = UIButton().then {
-        $0.backgroundColor = .white
-        $0.setTitle("찜한 상품", for: .normal)
-        $0.titleLabel!.font = .init(name: fontMedium, size: 15)
-    }
-    
-    private lazy var btn3 = UIButton().then {
-        $0.backgroundColor = .white
-        $0.setTitle("거래 내역", for: .normal)
-        $0.titleLabel!.font = .init(name: fontMedium, size: 15)
-    }
     
     private lazy var collectionView = UICollectionView().then {
         $0.backgroundColor = .white
@@ -134,6 +118,7 @@ class MyPageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
         view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
@@ -142,9 +127,7 @@ class MyPageViewController: UIViewController {
         setNavigationItem()
     }
     
-    private func setupView() {
-        view.addSubview(profileImg)
-    }
+    
     
     private func setNavigationItem(){
         navigationItem.title = "마이페이지"
@@ -156,16 +139,128 @@ class MyPageViewController: UIViewController {
     private func rightBarBtnDidTap() {
         
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupView() {
+        view.addSubview(profileImg)
+        view.addSubview(nameLabel)
+        view.addSubview(gearBtn)
+        view.addSubview(introduceLabel)
+        view.addSubview(productView)
+        view.addSubview(productNum)
+        view.addSubview(productLabel)
+        view.addSubview(prizeView)
+        view.addSubview(prizeNum)
+        view.addSubview(prizeLabel)
+        view.addSubview(receiveView)
+        view.addSubview(receiveNum)
+        view.addSubview(receiveLabel)
+        view.addSubview(transactionView)
+        view.addSubview(transactionNum)
+        view.addSubview(transactionLabel)
+        view.addSubview(collectionView)
+        
+        self.profileImg.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(26)
+            $0.leading.lessThanOrEqualToSuperview().offset(40)
+            $0.width.height.equalTo(80)
+        }
+        
+        self.nameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(32)
+            $0.leading.equalTo(self.profileImg.snp.trailing).offset(23)
+        }
+        
+        self.gearBtn.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(32)
+            $0.leading.equalTo(self.nameLabel.snp.trailing).offset(7)
+            $0.height.width.equalTo(14)
+        }
+        
+        self.introduceLabel.snp.makeConstraints {
+            $0.top.equalTo(self.nameLabel.snp.bottom).offset(5)
+            $0.leading.equalTo(self.profileImg.snp.trailing).offset(23)
+        }
+        
+        self.productView.snp.makeConstraints {
+            $0.top.equalTo(self.profileImg.snp.bottom).offset(19)
+            $0.leading.lessThanOrEqualToSuperview().offset(47)
+            $0.width.equalTo(48)
+            $0.height.equalTo(76)
+        }
+        
+        self.productNum.snp.makeConstraints {
+            $0.top.equalTo(self.productView.snp.top).offset(12)
+            $0.leading.lessThanOrEqualTo(self.productView.snp.leading).offset(18)
+            $0.trailing.greaterThanOrEqualTo(self.productView.snp.trailing).offset(-19)
+        }
+        
+        self.productLabel.snp.makeConstraints {
+            $0.top.equalTo(self.productNum.snp.bottom).offset(13)
+            $0.leading.lessThanOrEqualTo(self.productView.snp.leading).offset(12)
+            $0.trailing.greaterThanOrEqualTo(self.productView.snp.leading).offset(-12)
+        }
+        
+        self.prizeView.snp.makeConstraints {
+            $0.top.equalTo(self.profileImg.snp.bottom).offset(19)
+            $0.leading.lessThanOrEqualTo(self.productView.snp.trailing).offset(20)
+            $0.width.equalTo(75)
+            $0.height.equalTo(76)
+        }
+        
+        self.prizeNum.snp.makeConstraints {
+            $0.top.equalTo(self.prizeView.snp.top).offset(12)
+            $0.leading.lessThanOrEqualTo(self.prizeView.snp.leading).offset(27)
+            $0.trailing.greaterThanOrEqualTo(self.prizeView.snp.trailing).offset(-27)
+        }
+        
+        self.prizeLabel.snp.makeConstraints {
+            $0.top.equalTo(self.prizeNum.snp.bottom).offset(13)
+            $0.leading.lessThanOrEqualTo(self.prizeView.snp.leading).offset(11)
+            $0.trailing.greaterThanOrEqualTo(self.prizeView.snp.trailing).offset(-11)
+        }
+        
+        self.receiveView.snp.makeConstraints {
+            $0.top.equalTo(self.profileImg.snp.bottom).offset(19)
+            $0.leading.lessThanOrEqualTo(self.prizeView.snp.trailing).offset(20)
+            $0.width.equalTo(63)
+            $0.height.equalTo(76)
+        }
+        
+        self.receiveNum.snp.makeConstraints {
+            $0.top.equalTo(self.receiveView.snp.top).offset(12)
+            $0.leading.lessThanOrEqualTo(self.receiveView.snp.leading).offset(21)
+            $0.trailing.greaterThanOrEqualTo(self.receiveView.snp.trailing).offset(-21)
+        }
+        
+        self.receiveLabel.snp.makeConstraints {
+            $0.top.equalTo(self.receiveNum.snp.bottom).offset(13)
+            $0.leading.lessThanOrEqualTo(self.receiveView.snp.leading).offset(11)
+            $0.trailing.greaterThanOrEqualTo(self.receiveView.snp.trailing).offset(-11)
+        }
+        
+        self.transactionView.snp.makeConstraints {
+            $0.top.equalTo(self.profileImg.snp.bottom).offset(19)
+            $0.leading.lessThanOrEqualTo(self.receiveView.snp.trailing).offset(20)
+            $0.trailing.greaterThanOrEqualToSuperview().offset(-47)
+            $0.width.equalTo(74)
+            $0.height.equalTo(76)
+        }
+        
+        self.transactionNum.snp.makeConstraints {
+            $0.top.equalTo(self.transactionView.snp.top).offset(12)
+            $0.leading.lessThanOrEqualTo(self.transactionView.snp.leading).offset(21)
+            $0.trailing.greaterThanOrEqualTo(self.transactionView.snp.trailing).offset(-21)
+        }
+        
+        self.transactionLabel.snp.makeConstraints {
+            $0.top.equalTo(self.transactionNum.snp.bottom).offset(13)
+            $0.leading.lessThanOrEqualTo(self.transactionView.snp.leading).offset(12)
+            $0.trailing.greaterThanOrEqualTo(self.transactionView.snp.trailing).offset(-12)
+        }
+        
     }
-    */
 
+}
+
+extension MyPageViewController: UICollectionViewDelegateFlowLayout {
+    
 }
