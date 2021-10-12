7501531fcf941edc4036993eec789f42da161e6a
KimKiYoung <rlarldud6482@naver.com>
2021-10-12 14:07:19 +0900

    [Update] Extension

--- 09/09/Extension/Extension.swift
+++ 09/09/Extension/Extension.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import UIKit
+
+extension MainViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return 16
+        }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+            return 16
+        }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+            let width = collectionView.frame.width / 2 - 1
+            let size = CGSize(width: width, height: width)
+        
+            return size
+        }
+
+}
+
+extension MyPageViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+}
+
+extension UIViewController {
+    func alert(title: String, action: ((UIAlertAction)->Void)?) {
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "네", style: .default, handler: action)
+        let cancel = UIAlertAction(title: "아니요", style: .cancel, handler: nil)
+        
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
+}
