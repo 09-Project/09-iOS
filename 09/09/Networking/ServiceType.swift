67f28e3eb128570c5de870a22a89867e66c86a46
KimKiYoung <rlarldud6482@naver.com>
2021-10-14 20:20:42 +0900

    [Update] ServiceType

---
+++ 09/09/Networking/ServiceType.swift
@@ -0,0 +1,32 @@
+//
+//  ServiceType.swift
+//  09
+//
+//  Created by 김기영 on 2021/10/13.
+//
+
+import Foundation
+import Alamofire
+import RxAlamofire
+import RxSwift
+
+class ServiceType {
+    let baseURL = "http://3.36.26.221:8080"
+    
+    typealias httpResults = Observable<(HTTPURLResponse, Data)>
+    
+    func resultData(_ api: API) -> httpResults {
+        return RxAlamofire.requestData(api.method(), baseURL + api.path(), parameters: api.param,
+                                    encoding: JSONEncoding.prettyPrinted, headers: api.header())
+    }
+}
+
+enum networkingResult: Int {
+    case ok = 200
+    case okay = 201
+    case wrongRq = 400
+    case tokenError = 401
+    case notFound = 404
+    case conflict = 409
+    case falut = 0
+}
