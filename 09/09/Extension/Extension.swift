d4405d7540a41eff3371657411b6efc99fe5ffe7
KimKiYoung <rlarldud6482@naver.com>
2021-10-15 14:09:01 +0900

    [Update] Extension

--- 09/09/Extension/Extension.swift
+++ 09/09/Extension/Extension.swift
@@ -1,35 +1,35 @@
-////
-////  Extension.swift
-////  09
-////
-////  Created by 김기영 on 2021/10/11.
-////
 //
-//import Foundation
-//import UIKit
+//  Extension.swift
+//  09
 //
+//  Created by 김기영 on 2021/10/11.
+//
+
+import Foundation
+import UIKit
+
 //extension MainViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource {
 //    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 //        <#code#>
 //    }
-//    
+//
 //    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 //        <#code#>
 //    }
-//    
+//
 //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
 //            return 16
 //        }
-//    
+//
 //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
 //            return 16
 //        }
-//    
+//
 //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 //
 //            let width = collectionView.frame.width / 2 - 1
 //            let size = CGSize(width: width, height: width)
-//        
+//
 //            return size
 //        }
 //
@@ -39,30 +39,40 @@
 //    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 //        <#code#>
 //    }
-//    
+//
 //    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 //        <#code#>
 //    }
-//    
+//
 //    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 //        <#code#>
 //    }
-//    
+//
 //    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 //        <#code#>
 //    }
-//    
-//    
-//}
 //
-//extension UIViewController {
-//    func alert(title: String, action: ((UIAlertAction)->Void)?) {
-//        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
-//        let ok = UIAlertAction(title: "네", style: .default, handler: action)
-//        let cancel = UIAlertAction(title: "아니요", style: .cancel, handler: nil)
-//        
-//        alert.addAction(ok)
-//        alert.addAction(cancel)
-//        present(alert, animated: true, completion: nil)
-//    }
+//
 //}
+
+extension UIViewController {
+    func alert(title: String, action: ((UIAlertAction)->Void)?) {
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "네", style: .default, handler: action)
+        let cancel = UIAlertAction(title: "아니요", style: .cancel, handler: nil)
+        
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func underLine(view: UIView, txt: UITextField?, color: String) {
+        let border = CALayer()
+        border.frame = CGRect(x: 0, y: view.frame.size.height,
+                              width: view.frame.width,
+                              height: 1)
+        border.backgroundColor = UIColor.init(named: color)?.cgColor
+        view.layer.addSublayer(border)
+        txt?.textColor = .init(named: color)
+    }
+}
