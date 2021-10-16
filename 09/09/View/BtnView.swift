555fedb3a1d7624540bb6c6bb45435b6907b93e2
KimKiYoung <rlarldud6482@naver.com>
2021-10-16 14:54:40 +0900

    [Create] BtnView

---
+++ 09/09/View/BtnView.swift
@@ -0,0 +1,26 @@
+//
+//  BtnView.swift
+//  09
+//
+//  Created by 김기영 on 2021/10/16.
+//
+
+import UIKit
+
+class BtnView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+
+    
+    private func setup() {
+        
+    }
+}
