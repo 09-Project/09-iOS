04ceed06f9a93e2dffd67b10e1bb83c628feda3e
KimKiYoung <rlarldud6482@naver.com>
2021-10-18 22:55:38 +0900

    회원 정보 모델

---
+++ 09/09/model/User/InformationModel.swift
@@ -0,0 +1,14 @@
+//
+//  InformationModel.swift
+//  09
+//
+//  Created by 김기영 on 2021/10/18.
+//
+
+import Foundation
+
+struct InformationModel: Codable {
+    var name = String()
+    var profileUrl = String()
+    var introduction = String()
+}
