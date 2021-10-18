4089cf9e106aa0c844fbda55a4b3b201bf4aeaa3
KimKiYoung <rlarldud6482@naver.com>
2021-10-18 22:55:24 +0900

    토큰 모델

--- 09/09/model/Auth/TokenModel.swift
+++ 09/09/model/Auth/TokenModel.swift
@@ -11,6 +11,10 @@ struct TokenModel: Codable {
     let accessToken: String
     let refreshToken: String
     
+    init(accessToken: String, refreshToken: String) {
+        self.accessToken = accessToken
+        self.refreshToken = refreshToken
+    }
 }
 
 struct Token {
