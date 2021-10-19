73d98b3ef9c2be53263590dd630409f6d078627a
KimKiYoung <rlarldud6482@naver.com>
2021-10-19 19:12:40 +0900

    마이페이지 뷰 모델 생성

---
+++ 09/09/ViewModel/Post/MyPageViewModel.swift
@@ -0,0 +1,24 @@
+////
+////  MyPageViewModel.swift
+////  09
+////
+////  Created by 김기영 on 2021/10/18.
+////
+//
+//import Foundation
+//import RxSwift
+//import RxCocoa
+//
+//struct MyPageViewModel: ViewModelType {
+//    private let disposebag = DisposeBag()
+//    
+//    struct input {
+//        let member_ID: Driver<Int>
+//        let doneTap: Driver<Void>
+//        let showInfo: Signal<Void>
+//    }
+//    
+//    struct output {
+//        let result: Signal<Bool>
+//    }
+//}
