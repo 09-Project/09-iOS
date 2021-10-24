62982fc5f5afa3d994fddcf50fb115876b731042
KimKiYoung <rlarldud6482@naver.com>
2021-10-24 18:46:10 +0900

    AppDelegate

--- 09/09/AppDelegate.swift
+++ 09/09/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        AuthAPI().refreshToken()
+        Service().refreshToken()
         return true
     }
 
@@ -33,7 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
-        AuthAPI().refreshToken()
+        Service().refreshToken()
     }
 
 
