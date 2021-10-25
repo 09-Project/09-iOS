f175b587509bd9bb9a3151b51934ce71598ca7d5
KimKiYoung <rlarldud6482@naver.com>
2021-10-25 20:24:34 +0900

    addBorder Extension 추가

--- 09/09/Extension/Extension.swift
+++ 09/09/Extension/Extension.swift
@@ -102,16 +102,6 @@ extension UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    func underLine(view: UIView, txt: UITextField?, color: String) {
-        let border = CALayer()
-        border.frame = CGRect(x: 0, y: view.frame.size.height,
-                              width: view.frame.width,
-                              height: 1)
-        border.backgroundColor = UIColor.init(named: color)?.cgColor
-        view.layer.addSublayer(border)
-        txt?.textColor = .init(named: color)
-    }
-    
     func line(view: UIView) {
         lazy var lineView1 = UIView().then {
         $0.backgroundColor = .init(named: "placeholderColor")
@@ -135,5 +125,31 @@ extension UIViewController {
             $0.width.equalToSuperview()
         }
         
+    }
+}
+
+extension CALayer {
+    func addBorder(_ arr_edge: [UIRectEdge], color: UIColor, width: CGFloat) {
+        for edge in arr_edge {
+            let border = CALayer()
+            switch edge {
+            case UIRectEdge.top:
+                border.frame = CGRect.init(x: 0, y: 0, width: frame.width, height: width)
+                break
+            case UIRectEdge.bottom:
+                border.frame = CGRect.init(x: 0, y: frame.height - width, width: frame.width, height: width)
+                break
+            case UIRectEdge.left:
+                border.frame = CGRect.init(x: 0, y: 0, width: width, height: frame.height)
+                break
+            case UIRectEdge.right:
+                border.frame = CGRect.init(x: frame.width - width, y: 0, width: width, height: frame.height)
+                break
+            default:
+                break
+            }
+            border.backgroundColor = color.cgColor;
+            self.addSublayer(border)
+        }
     }
 }
