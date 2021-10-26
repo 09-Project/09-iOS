3e7e4bd52107d0dc8dce6cbe99c524f84e2f5535
KimKiYoung <rlarldud6482@naver.com>
2021-10-27 08:00:13 +0900

    PostViewModel 수정

--- 09/09/ViewModel/Post/PostViewModel.swift
+++ 09/09/ViewModel/Post/PostViewModel.swift
@@ -1,48 +1,41 @@
 //
-//  PostViewModel.swift
+//  ViewModel.swift
 //  09
 //
-//  Created by 김기영 on 2021/10/24.
+//  Created by 김기영 on 2021/10/26.
 //
 
 import Foundation
-import RxCocoa
 import RxSwift
+import RxCocoa
 
 class PostViewModel: ViewModelType {
-    
     private let disposebag = DisposeBag()
+    
     struct Input {
-        let title: Driver<String>
-        let content: Driver<String>
-        let price: Int?
-        let transactionRegion: Driver<String>
-        let openChatLink: Driver<String>
-        let image: Driver<String>
-        let doneTap: Signal<Void>
+        let getPost: Driver<Void>
     }
     
-    struct Output{
-        let isEnable: Driver<Bool>
-        let result: Signal<Bool>
+    struct Output {
+        let getPostResult: PublishRelay<Bool>
+        let post: BehaviorRelay<[PostModel]>
     }
     
     func transform(_ input: Input) -> Output {
         let api = Service()
-        let result = PublishSubject<Bool>()
-        let info = Driver.combineLatest(input.title, input.content, input.transactionRegion,
-                                        input.openChatLink, input.image)
-        let isEnabel = info.map { !$0.0.isEmpty && !$0.1.isEmpty && !$0.2.isEmpty && !$0.3.isEmpty
-            && !$0.4.isEmpty}
+        let getPostResult = PublishRelay<Bool>()
+        let post = BehaviorRelay<[PostModel]>(value: [])
         
-        input.doneTap.asObservable()
-            .withLatestFrom(info)
-            .flatMap { title, content, transactionRegion, openChatLink, image in
-                api.post(title: title, content: content, price: input.price ?? 0, transactionRegion: transactionRegion, openChatLink: openChatLink, image: image)
-            }.subscribe(onNext: { _ in
-                result.onCompleted()
-            }).disposed(by: disposebag)
+        input.getPost.asObservable().flatMap{_ in api.products(page: 8, size: 4)}
+        .subscribe(onNext: { data, res in
+            switch res {
+            case .ok:
+                post.accept(data!.PostList)
+            default:
+                getPostResult.accept(false)
+            }
+        }).disposed(by: disposebag)
         
-        return Output(isEnable: isEnabel.asDriver(onErrorJustReturn: false), result: result.asSignal(onErrorJustReturn: false))
+        return Output(getPostResult: getPostResult, post: post)
     }
 }
