3fd2bc5435ac85c2ac243d4e6cefa4a974db7921
KimKiYoung <rlarldud6482@naver.com>
2021-11-01 08:20:19 +0900

    MainVieweModel

---
+++ 09/09/Source/ViewModel/MainViewModel.swift
@@ -0,0 +1,75 @@
+//
+//  MainViewModel.swift
+//  09
+//
+//  Created by 김기영 on 2021/10/31.
+//
+import Foundation
+import RxSwift
+import RxCocoa
+
+class MainViewModel: ViewModelType {
+    
+    private let disposebag = DisposeBag()
+    
+    struct Input {
+        let getPost: Signal<Void>
+        let getMorePost: Signal<Void>
+        let getBackPost: Signal<Void>
+    }
+    
+    struct Output {
+        let getPostResult: PublishRelay<Bool>
+        let post: BehaviorRelay<[PostModel]>
+    }
+    
+    func transform(_ input: Input) -> Output {
+        let api = Service()
+        let getPostResult = PublishRelay<Bool>()
+        let post = BehaviorRelay<[PostModel]>(value: [])
+        var page = 0
+        
+        input.getPost.asObservable().flatMap{ _ in api.products(page: 0, size: 16)}
+        .subscribe(onNext: { data, res in
+            switch res {
+            case .ok:
+                post.accept(data!.PostList)
+                getPostResult.accept(true)
+            default:
+                print(res)
+                getPostResult.accept(false)
+            }
+        }).disposed(by: disposebag)
+        
+        input.getMorePost.asObservable().map{ page += 1 }.flatMap{ _ in
+            api.products(page: page, size: 16)
+        }.subscribe(onNext: { data, res in
+            switch res {
+            case .ok:
+                post.accept(data!.PostList)
+                getPostResult.accept(true)
+            default:
+                print(res)
+                getPostResult.accept(false)
+            }
+        }).disposed(by: disposebag)
+        
+        input.getBackPost.asObservable().map{ page -= 1}.flatMap{ _ in
+            api.products(page: page, size: 16)
+        }.subscribe(onNext: { data, res in
+            switch res {
+            case .ok:
+                post.accept(data!.PostList)
+                getPostResult.accept(true)
+            default:
+                print(res)
+                getPostResult.accept(false)
+            }
+        }).disposed(by: disposebag)
+        
+        
+        return Output(getPostResult: getPostResult, post: post)
+    }
+}
+
+
