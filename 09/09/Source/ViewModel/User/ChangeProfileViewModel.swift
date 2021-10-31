273d2de5c5878891a918b2c361cc1c991d2cebd2
KimKiYoung <rlarldud6482@naver.com>
2021-11-01 08:20:55 +0900

    ChangeProfileViewModel 수정

--- 09/09/Source/ViewModel/User/ChangeProfileViewModel.swift
+++ 09/09/Source/ViewModel/User/ChangeProfileViewModel.swift
@@ -1,27 +1,45 @@
-////
-////  ChangeProfileViewModel.swift
-////  09
-////
-////  Created by 김기영 on 2021/10/28.
-////
 //
-//import Foundation
-//import RxSwift
-//import RxCocoa
+//  ChangeProfileViewModel.swift
+//  09
 //
-//class ChangeProfileViewModel: ViewModelType {
-//    
-//    struct Input {
-//        let image: Driver<String>
-//        let name: Driver<String>
-//    }
-//    
-//    struct Output {
-//        let result: Signal<String>
-//    }
-//    
-//    func transform(_ input: Input) -> Output {
-//        let api = Service()
-//        let result = PublishSubject<String>()
-//    }
-//}
+//  Created by 김기영 on 2021/10/28.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class ChangeProfileViewModel: ViewModelType {
+    
+    private let bag = DisposeBag()
+    
+    struct Input {
+        let name: Driver<String>
+        let introduction: Driver<String>
+        let profileURL: Driver<String>
+        let doneTap: Driver<Void>
+    }
+    
+    struct Output {
+        let result: Signal<Bool>
+    }
+    
+    func transform(_ input: Input) -> Output {
+        let api = Service()
+        let result = PublishSubject<Bool>()
+        let info = Driver.combineLatest(input.name, input.introduction, input.profileURL)
+        
+        input.doneTap.asObservable().withLatestFrom(info).flatMap{ name, introduction, profileUrl in
+            api.changeInformation(name, introduction, profileUrl)
+        }.subscribe(onNext: { res in
+            switch res {
+            case .ok:
+                result.onNext(true)
+            default:
+                result.onNext(false)
+            }
+        }).disposed(by: bag)
+        
+        return Output(result: result.asSignal(onErrorJustReturn: false))
+    }
+}
