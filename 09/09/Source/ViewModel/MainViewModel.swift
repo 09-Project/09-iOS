47d1828c120734c48c9fd03fc12d8cfb5e5815bd
KimKiYoung <rlarldud6482@naver.com>
2021-11-01 10:00:22 +0900

    MainViewModel Search 기능 추가

--- 09/09/Source/ViewModel/MainViewModel.swift
+++ 09/09/Source/ViewModel/MainViewModel.swift
@@ -16,24 +16,30 @@ class MainViewModel: ViewModelType {
         let getPost: Signal<Void>
         let getMorePost: Signal<Void>
         let getBackPost: Signal<Void>
+        let searchBtn: Signal<Void>
+        let searchTxt: Signal<String?>
+//        let heartBtnTap: Signal<Void>
+//        let postId: Driver<Int>
     }
     
     struct Output {
         let getPostResult: PublishRelay<Bool>
         let post: BehaviorRelay<[PostModel]>
+//        let getHearResult: PublishRelay<Bool>
     }
     
     func transform(_ input: Input) -> Output {
         let api = Service()
         let getPostResult = PublishRelay<Bool>()
         let post = BehaviorRelay<[PostModel]>(value: [])
+//        let getHeartResult = PublishRelay<Bool>()
         var page = 0
         
         input.getPost.asObservable().flatMap{ _ in api.products(page: 0, size: 16)}
         .subscribe(onNext: { data, res in
             switch res {
             case .ok:
-                post.accept(data!.PostList)
+                post.accept(data!.posts)
                 getPostResult.accept(true)
             default:
                 print(res)
@@ -42,11 +48,11 @@ class MainViewModel: ViewModelType {
         }).disposed(by: disposebag)
         
         input.getMorePost.asObservable().map{ page += 1 }.flatMap{ _ in
-            api.products(page: page, size: 16)
+            api.products(page: page, size: 8)
         }.subscribe(onNext: { data, res in
             switch res {
             case .ok:
-                post.accept(data!.PostList)
+                post.accept(data!.posts)
                 getPostResult.accept(true)
             default:
                 print(res)
@@ -55,11 +61,11 @@ class MainViewModel: ViewModelType {
         }).disposed(by: disposebag)
         
         input.getBackPost.asObservable().map{ page -= 1}.flatMap{ _ in
-            api.products(page: page, size: 16)
+            api.products(page: page, size: 8)
         }.subscribe(onNext: { data, res in
             switch res {
             case .ok:
-                post.accept(data!.PostList)
+                post.accept(data!.posts)
                 getPostResult.accept(true)
             default:
                 print(res)
@@ -67,6 +73,27 @@ class MainViewModel: ViewModelType {
             }
         }).disposed(by: disposebag)
         
+        input.searchBtn.asObservable().withLatestFrom(input.searchTxt).flatMap{ text in
+            api.search(keywords: text, page: page, size: 8)
+        }.subscribe(onNext: { data, res in
+            switch res {
+            case .ok:
+                post.accept(data!.posts)
+            default:
+                print(res)
+            }
+        }).disposed(by: disposebag)
+        
+//        input.heartBtnTap.asObservable().withLatestFrom(input.postId).flatMap{ id in
+//            api.like(id)
+//        }.subscribe(onNext: { res in
+//            switch res {
+//            case .ok:
+//                getHeartResult.accept(true)
+//            default:
+//                getHeartResult.accept(false)
+//            }
+//        }).disposed(by: disposebag)
         
         return Output(getPostResult: getPostResult, post: post)
     }
