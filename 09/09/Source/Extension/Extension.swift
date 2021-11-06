9ba70af9f8642b68f55f7c5aa3e11d13f352c782
KimKiYoung <rlarldud6482@naver.com>
2021-11-06 16:56:43 +0900

    okAlert 추가

--- 09/09/Source/Extension/Extension.swift
+++ 09/09/Source/Extension/Extension.swift
@@ -97,8 +97,14 @@ extension UIViewController {
         let ok = UIAlertAction(title: "네", style: .default, handler: action)
         let cancel = UIAlertAction(title: "아니요", style: .destructive, handler: nil)
         
+        [ok, cancel].forEach{ alert.addAction($0) }
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func okAlert(title: String, action: ((UIAlertAction)->Void)?) {
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "확인", style: .default, handler: action)
         alert.addAction(ok)
-        alert.addAction(cancel)
         present(alert, animated: true, completion: nil)
     }
     
