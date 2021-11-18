e2daaa0732471725b2400a11cfec13710d35e72e
KimKiYoung <rlarldud6482@naver.com>
2021-11-18 19:14:51 +0900

    프로필 뷰 모델 수정

--- 09/09/Source/ViewModel/User/ProfileViewModel.swift
+++ 09/09/Source/ViewModel/User/ProfileViewModel.swift
@@ -6,3 +6,75 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+struct ProfileViewModel: ViewModelType {
+    
+    private let disposebag = DisposeBag()
+    
+    struct Input {
+        let getUserInfo: Signal<Void>
+        let getPost: Signal<Void>
+        let getLikePost: Signal<Void>
+        let getDetail: Signal<Void>
+        let memberID: Int
+    }
+    
+    struct Output {
+        let myInfo: PublishRelay<ProfileModel?>
+        let post: BehaviorRelay<[PostModel]>
+    }
+    
+    func transform(_ input: Input) -> Output {
+        let api = Service()
+        let userInfo = PublishRelay<ProfileModel?>()
+        let post = BehaviorRelay<[PostModel]>(value: [])
+        
+        input.getUserInfo.asObservable().flatMap { _ in
+            api.profile(input.memberID)
+        }.subscribe(onNext: { data, res in
+            switch res {
+            case .ok:
+                userInfo.accept(data!)
+            default:
+                userInfo.accept(nil)
+            }
+        }).disposed(by: disposebag)
+        
+        input.getPost.asObservable().flatMap {
+            _ in api.products(page: 0)
+        }.subscribe(onNext: { data, res in
+            switch res {
+            case .ok:
+                post.accept(data!.posts)
+            default:
+                print(data)
+            }
+        }).disposed(by: disposebag)
+        
+        input.getLikePost.asObservable().flatMap {
+            _ in api.seeLikePost()
+        }.subscribe(onNext: { data, res in
+            switch res {
+            case .ok:
+                post.accept(data!)
+            default:
+                print(data)
+            }
+        }).disposed(by: disposebag)
+        
+        input.getDetail.asObservable().flatMap { _ in
+            api.seeDeletePost(input.memberID)
+        }.subscribe(onNext: { data, res in
+            switch res {
+            case .ok:
+                post.accept(data!)
+            default:
+                print(data)
+            }
+        }).disposed(by: disposebag)
+        
+        return Output(myInfo: userInfo, post: post)
+    }
+}
